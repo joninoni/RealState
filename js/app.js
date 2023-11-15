970ecc6a820e43263dc37ce450024674403dcefc
joninoni <jonibeltranhayer@gmail.com>
2023-11-15 08:08:05 -0600

    finalizando proyecto de realstate

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const imagenes=document.querySelectorAll(".propiedad__imagen");
+
+window.addEventListener("scroll",()=>{
+    const scroll=this.scrollY /-20;
+    imagenes.forEach((imagen)=>{
+        if(scroll <-80){
+            scroll+=80;
+        }
+        imagen.style.backgroundPositionY=`${scroll}px`
+    })
+})
